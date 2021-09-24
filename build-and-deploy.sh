32a8d036c3769e221bbb1c972ceb6b95890d9e62
Dave Smith-Uchida <dsmithuchida@vmware.com>
2021-09-24 14:41:05 -0700

    Added astrolabe_repo_server to run a Kopia Astrolabe server and added stuff for AWS clusters with Shepherd (WIP)

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -4,11 +4,14 @@ cd cmd/astrolabe_server
 LD_LIBRARY_PATH=/usr/local/vmware-vix-disklib-distrib/lib64 go build
 mkdir -p ../../docker/astrolabe_server/bin
 cp astrolabe_server ../../docker/astrolabe_server/bin/astrolabe_server
+cd ../astrolabe_repo_server
+LD_LIBRARY_PATH=/usr/local/vmware-vix-disklib-distrib/lib64 go build
+mkdir -p ../../dsu-igeek/astrolabe-demo/docker/astrolabe_repo_server/bin
+cp astrolabe_repo_server ../../dsu-igeek/astrolabe-demo/docker/astrolabe_repo_server/bin/astrolabe_repo_server
 cd ../../../../minio/minio/
 LD_LIBRARY_PATH=/usr/local/vmware-vix-disklib-distrib/lib64 make
 mkdir -p ../../dsu-igeek/astrolabe-demo/docker/astrolabe_minio/bin
 cp minio ../../dsu-igeek/astrolabe-demo/docker/astrolabe_minio/bin/minio
-
 TAG=`date '+%b-%d-%Y-%H-%M-%S'`
 
 cd ../../dsu-igeek/astrolabe-demo/docker/astrolabe_server
@@ -19,3 +22,6 @@ cd ../astrolabe_minio
 docker build -t dsmithuchida/astrolabe_minio:$TAG .
 docker push dsmithuchida/astrolabe_minio:$TAG
 
+cd ../astrolabe_repo_server
+docker build -t dsmithuchida/astrolabe_repo_server:$TAG .
+docker push dsmithuchida/astrolabe_repo_server:$TAG
--- go.mod
+++ go.mod
@@ -7,11 +7,9 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/vmware-tanzu/astrolabe v0.3.0
-	github.com/zalando/postgres-operator v1.6.0
-	k8s.io/apimachinery v0.19.7
-	k8s.io/client-go v0.19.7
+	github.com/zalando/postgres-operator v1.6.3
+	k8s.io/apimachinery v0.20.6
+	k8s.io/client-go v0.20.6
 )
 
 replace github.com/vmware-tanzu/astrolabe => ../../vmware-tanzu/astrolabe
-
-replace github.com/vmware-tanzu/velero => ../../vmware-tanzu/velero
