ad5a14c7f0ff3eb538e12884d995f2888dfa93bb
Dave Smith-Uchida <dsmithuchida@vmware.com>
2021-09-24 14:41:05 -0700

    Added astrolabe_repo_server to run a Kopia Astrolabe server and added stuff for AWS clusters with Shepherd (WIP)

---
+++ cmd/astrolabe_repo_server/main.go
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2019 the Astrolabe contributors
+ * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"github.com/dsu-igeek/astrolabe-demo/pkg/psql"
+	kubernetes "github.com/vmware-tanzu/astrolabe-velero/pkg/k8sns"
+	"github.com/vmware-tanzu/astrolabe/pkg/server"
+	ebs_astrolabe "github.com/vmware-tanzu/velero-plugin-for-aws/pkg/ebs-astrolabe"
+)
+
+func main() {
+	addOnInits := make(map[string]server.InitFunc)
+	addOnInits["psql"] = psql.NewPSQLProtectedEntityTypeManager
+	addOnInits["ebs"] = ebs_astrolabe.NewEBSProtectedEntityTypeManager
+	addOnInits["k8sns"] = kubernetes.NewKubernetesNamespaceProtectedEntityTypeManagerFromConfig
+	ServerMain(addOnInits)
+}
--- cmd/astrolabe_server/go.mod
+++ cmd/astrolabe_server/go.mod
@@ -4,10 +4,9 @@ go 1.14
 
 require (
 	github.com/dsu-igeek/astrolabe-demo v0.0.0-00010101000000-000000000000
-	github.com/vmware-tanzu/astrolabe v0.4.0
+	github.com/vmware-tanzu/astrolabe v0.4.1-0.20210813185044-12eb18c3f6d5
 	github.com/vmware-tanzu/astrolabe-velero v0.0.0-00010101000000-000000000000
 	github.com/vmware-tanzu/velero-plugin-for-aws v0.0.0-00010101000000-000000000000
-	github.com/vmware-tanzu/velero-plugin-for-vsphere v1.2.1 // indirect
 )
 
 replace github.com/vmware-tanzu/astrolabe => ../../../../vmware-tanzu/astrolabe
@@ -25,3 +24,5 @@ replace github.com/vmware-tanzu/velero-plugin-for-aws => ../../../../vmware-tanz
 replace github.com/vmware-tanzu/velero-plugin-for-vsphere => ../../../../vmware-tanzu/velero-plugin-for-vsphere
 
 replace github.com/vmware-tanzu/astrolabe-velero => ../../../../vmware-tanzu/astrolabe-velero
+
+replace github.com/zalando/postgres-operator => ../../../../zalando/postgres-operator
