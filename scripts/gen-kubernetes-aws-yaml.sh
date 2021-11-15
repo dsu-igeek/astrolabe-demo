40ff0115a6b8ce243abb2c1e8e05eaf04fd9fa0f
Dave Smith-Uchida <dsmithuchida@vmware.com>
2021-11-15 07:32:20 +0000

    Fixed S3 config w/multiple external IPs, added better AWS support

---
+++ scripts/gen-kubernetes-aws-yaml.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# usage: install-with-kustomize.sh <cluster tag> <astrolabe container tag>
+BASE=`pwd`
+cd $1
+mkdir -p yaml
+sed -e "s/latest/$2/" < $BASE/../k8s/astrolabe-server/astrolabe-deployment.yaml > ./yaml/astrolabe-deployment.yaml
+cp $BASE/../k8s/astrolabe-server/astrolabe-storage-class.aws.yaml ./yaml
+cp $BASE/../k8s/astrolabe-server/astrolabe-service-account.yaml ./yaml
--- scripts/install-kubernetes-aws-yaml.sh
+++ scripts/install-kubernetes-aws-yaml.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd $1
 kubectl delete --kubeconfig kube/config -n astrolabe -f yaml/astrolabe-storage-class.yaml
-kubectl apply --kubeconfig kube/config -n astrolabe -f yaml/astrolabe-storage-class.yaml
+kubectl apply --kubeconfig kube/config -n astrolabe -f yaml/astrolabe-storage-class.aws.yaml
 kubectl apply --kubeconfig kube/config -n astrolabe -f yaml/astrolabe-service-account.yaml 
 kubectl apply --kubeconfig kube/config -n astrolabe -f yaml/astrolabe-deployment.yaml 
